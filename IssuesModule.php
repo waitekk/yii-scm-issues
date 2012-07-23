4b7a61e6c14ea6234d89acf6471e4a14aaf0eef4
Max V. Zaets <waitekk.work@gmail.com>
2012-07-23 17:27:21 +0600

    Added initial settings

--- IssuesModule.php
+++ IssuesModule.php
@@ -1,8 +1,37 @@
 <?php
-
+/**
+ * This module allows you to set up a page in a restricted
+ * area of your web site. On this page users can communicate with you
+ * through issues, which are hosted on a Bitbucket repo,
+ * associated with your website.
+ *
+ * @author Max Zaets <waitekk.work@gmail.com>
+ * @copyright Copyright &copy; Max Zaets 2012-
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
 class IssuesModule extends CWebModule
 {
-	public function init()
+    /**
+     * Your Bitbucket username
+     */
+    public $username;
+
+    /**
+     * Your Bitbucket Password
+     */
+    public $password;
+
+    /**
+     * Your Bitbucket repo slug (see https://confluence.atlassian.com/display/BITBUCKET/What+is+a+Slug)
+     */
+    public $repo_slug;
+
+    /**
+     * Hide issues with this type set
+     */
+    public $hide_issues = array('resolved', 'wontfix', 'duplicate', 'invalid');
+
+    public function init()
 	{
 		// this method is called when the module is being created
 		// you may place code here to customize the module or the application
